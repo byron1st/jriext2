989fbfdd92255fecfa51206f5503b34115ac3009
byron1st <byron1st@icloud.com>
2017-06-24 09:57:45 +0900

    parser 준비

---
+++ src/main/java/edu/kaist/salab/byron1st/jriext2/loggingtoolset/LogFilesCreationFailedException.java
@@ -0,0 +1,7 @@
+package edu.kaist.salab.byron1st.jriext2.loggingtoolset;
+
+/**
+ * Created by byron1st on 2017. 6. 24..
+ */
+public class LogFilesCreationFailedException {
+}
---
+++ src/main/java/edu/kaist/salab/byron1st/jriext2/loggingtoolset/ParserAppImplJSON.java
@@ -0,0 +1,17 @@
+package edu.kaist.salab.byron1st.jriext2.loggingtoolset;
+
+import org.json.JSONArray;
+
+import java.io.File;
+
+/**
+ * Created by byron1st on 2017. 6. 24..
+ */
+public class ParserAppImplJSON implements ParserApp<JSONArray> {
+    @Override
+    public JSONArray parse(File outputFile) {
+
+
+        return null;
+    }
+}
