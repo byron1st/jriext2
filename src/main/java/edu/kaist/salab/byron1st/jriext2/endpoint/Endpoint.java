7a63fbcc0ec7e6c7f03a337d61945b220854c76a
byron1st <byron1st@icloud.com>
2017-06-24 11:34:11 +0900

    parser 제거. 분리할 예정.

--- src/main/java/edu/kaist/salab/byron1st/jriext2/endpoint/Endpoint.java
+++ src/main/java/edu/kaist/salab/byron1st/jriext2/endpoint/Endpoint.java
@@ -15,27 +15,11 @@ import java.util.HashMap;
  */
 
 public class Endpoint implements Symbols {
-
-
     public static void instrument(Path targetClassPath, ArrayList<ETType> ettypeList) throws ClassReaderNotConstructedException, CopyingNotInstClassesFailedException, WritingInstrumentedClassFailedException, CopyJRiExtLoggerClassFileFailedException {
         InstApp.getInstance().instrument(targetClassPath, ettypeList);
     }
 
     public static String execute(String mainClassName, Path outputFilePath, Path errorFilePath) throws TargetSystemExecutionFailedException, RequiredFilesNotExistException, LogFilesCreationFailedException {
         return ExecuterApp.getInstance().execute(mainClassName, outputFilePath, errorFilePath);
-    }
-
-    public static HashMap<ParseType, Object> parse(File outputFile, ArrayList<ParseType> parseTypeList) {
-        HashMap<ParseType, Object> parseResults = new HashMap<>();
-        parseTypeList.forEach(parseType -> parseResults.put(parseType, runParserImpl(outputFile, parseType)));
-
-        return parseResults;
-    }
-
-    private static Object runParserImpl(File outputFile, ParseType parseType) {
-        switch (parseType) {
-            case JSON: return (new ParserAppImplJSON()).parse(outputFile);
-            default: return null;
-        }
     }
 }
