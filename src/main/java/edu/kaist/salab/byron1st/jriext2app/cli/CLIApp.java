11dc6acedef48528c26792dc2722a03aa3ad2452
byron1st <byron1st@icloud.com>
2017-07-02 15:39:51 +0900

    exec 작동하게 코드 작성

--- src/main/java/edu/kaist/salab/byron1st/jriext2app/cli/CLIApp.java
+++ src/main/java/edu/kaist/salab/byron1st/jriext2app/cli/CLIApp.java
@@ -1,8 +1,14 @@
 package edu.kaist.salab.byron1st.jriext2app.cli;
 
+import edu.kaist.salab.byron1st.jriext2.Endpoint;
 import edu.kaist.salab.byron1st.jriext2.ettype.ETType;
-import edu.kaist.salab.byron1st.jriext2.executer.ExecuterApp;
-import edu.kaist.salab.byron1st.jriext2.inst.*;
+import edu.kaist.salab.byron1st.jriext2.executer.LogFilesCreationFailedException;
+import edu.kaist.salab.byron1st.jriext2.executer.RequiredFilesNotExistException;
+import edu.kaist.salab.byron1st.jriext2.executer.TargetSystemExecutionFailedException;
+import edu.kaist.salab.byron1st.jriext2.inst.ClassReaderNotConstructedException;
+import edu.kaist.salab.byron1st.jriext2.inst.InstrumentedClassWriteFailedException;
+import edu.kaist.salab.byron1st.jriext2.inst.JRiExtLoggerClassFileCopyFailedException;
+import edu.kaist.salab.byron1st.jriext2.inst.NotInstClassesCopyFailedException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +17,7 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -18,16 +25,29 @@ import java.util.Scanner;
  */
 public class CLIApp {
     public static final String KEY_ERROR = "error";
-    public static final String KEY_DONE = "done";
-    public static final String MESSAGE_INST = "inst";
-    public static final String MESSAGE_EXEC = "exec";
-    public static final String MESSAGE_QUIT = "quit";
+    public static final String KEY_DONE_INST = "done.inst";
+    public static final String KEY_DONE_EXEC = "done.exec";
+    public static final String KEY_TERM_EXEC = "term.exec";
+    public static final String KEY_DONE_QUIT = "done.quit";
     public static final String CMD_QUIT = "cmd.quit";
     public static final String CMD_INST = "cmd.inst";
     public static final String CMD_EXEC = "cmd.exec";
     public static final String CMD_STOP = "cmd.stop";
 
+    public static void send(String key, String... args) {
+        JSONObject messageObject = new JSONObject();
+        messageObject.put("key", key);
+        if(args != null) {
+            messageObject.put("body", args);
+        }
+
+        System.out.println(messageObject.toString());
+    }
+
     public static void main(String[] args) {
+        HashMap<String, String> mapProcessKeyToUniqueName = new HashMap<>();
+        ProcessObserver observer = new ProcessObserver(mapProcessKeyToUniqueName);
+        Endpoint.setProcessStatusObserver(observer);
         /**
          * command:
          * {
@@ -43,20 +63,23 @@ public class CLIApp {
                 String command = commandObject.getString("cmd");
                 switch (command) {
                     case CMD_QUIT:
-                        send(KEY_DONE, MESSAGE_QUIT);
+                        send(KEY_DONE_QUIT, null);
                         break label;
                     case CMD_INST:
                         instrument(commandObject.getJSONArray("args"));
-                        send(KEY_DONE, MESSAGE_INST);
+                        send(KEY_DONE_INST, null);
                         break;
                     case CMD_EXEC:
-                        execute(commandObject.getJSONArray("args"));
-                        send(KEY_DONE, MESSAGE_EXEC);
+                        JSONArray execargs = commandObject.getJSONArray("args");
+                        String uniqueName = execargs.getString(1);
+                        String processKey = execute(execargs);
+                        mapProcessKeyToUniqueName.put(processKey, uniqueName);
+                        send(KEY_DONE_EXEC, uniqueName, processKey);
                         break;
                     case CMD_STOP:
                         break;
                 }
-            } catch (JRiExtLoggerClassFileCopyFailedException | WrongArgumentsException | NotInstClassesCopyFailedException | InstrumentedClassWriteFailedException | ClassReaderNotConstructedException | JSONException e) {
+            } catch (JRiExtLoggerClassFileCopyFailedException | WrongArgumentsException | NotInstClassesCopyFailedException | InstrumentedClassWriteFailedException | ClassReaderNotConstructedException | JSONException | RequiredFilesNotExistException | LogFilesCreationFailedException | TargetSystemExecutionFailedException e) {
                 send(KEY_ERROR, e.getMessage());
             }
         }
@@ -64,29 +87,36 @@ public class CLIApp {
 
     private static void instrument(JSONArray args) throws JRiExtLoggerClassFileCopyFailedException, ClassReaderNotConstructedException, InstrumentedClassWriteFailedException, NotInstClassesCopyFailedException, WrongArgumentsException {
         Path targetClassPath;
-//        Path ettypeDefFilePath;
         JSONArray ettypeInfoList;
         try {
             targetClassPath = Paths.get(args.getString(0));
-//            ettypeDefFilePath = Paths.get(args.getString(1));
             ettypeInfoList = args.getJSONArray(1);
 
-//            ArrayList<ETType> ettypeList = (new ETTypeBuilderImplJson()).buildETTypeList(ettypeDefFilePath);
             ArrayList<ETType> ettypeList = (new ETTypeBuilderImplJson()).buildETTypeList(ettypeInfoList);
-            InstApp.getInstance().instrument(targetClassPath, ettypeList);
-        } catch (ArrayIndexOutOfBoundsException | InvalidPathException e) {
+            Endpoint.instrument(targetClassPath, ettypeList);
+        } catch (ArrayIndexOutOfBoundsException e) {
             throw new WrongArgumentsException("Arguments are wrong.", e);
+        } catch (InvalidPathException e) {
+            throw new WrongArgumentsException("Classpath is wrong.", e);
         }
     }
 
-    private static void execute(JSONArray args) {
-//        ExecuterApp.getInstance().execute()
-    }
+    private static String execute(JSONArray args) throws RequiredFilesNotExistException, LogFilesCreationFailedException, TargetSystemExecutionFailedException, WrongArgumentsException {
+        String mainClassName = args.getString(0);
+        Path outputFilePath = null;
+        Path errorFilePath = null;
+        try {
+            if(!args.isNull(2)) {
+                outputFilePath = Paths.get(args.getString(1));
+            }
 
-    private static void send(String key, String message) {
-        JSONObject messageObject = new JSONObject();
-        messageObject.put("key", key);
-        messageObject.put("body", message);
-        System.out.println(messageObject.toString());
+            if(!args.isNull(3)) {
+                errorFilePath = Paths.get(args.getString(2));
+            }
+        } catch (InvalidPathException e) {
+            throw new WrongArgumentsException("Output paths are wrong.", e);
+        }
+
+        return Endpoint.execute(mainClassName, outputFilePath, errorFilePath);
     }
 }
--- src/main/java/edu/kaist/salab/byron1st/jriext2app/cli/ProcessObserver.java
+++ src/main/java/edu/kaist/salab/byron1st/jriext2app/cli/ProcessObserver.java
@@ -3,12 +3,23 @@ package edu.kaist.salab.byron1st.jriext2app.cli;
 import edu.kaist.salab.byron1st.jriext2.Symbols;
 import edu.kaist.salab.byron1st.jriext2.executer.ProcessStatusObserver;
 
+import java.util.HashMap;
+
 /**
  * Created by byron1st on 2017. 6. 28..
  */
 public class ProcessObserver implements ProcessStatusObserver, Symbols {
+    public HashMap<String, String> mapProcessKeyToUniqueName = new HashMap<>();
+
+    public ProcessObserver (HashMap<String, String> mapProcessKeyToUniqueName) {
+        this.mapProcessKeyToUniqueName = mapProcessKeyToUniqueName;
+    }
+
     @Override
     public void observe(String processKey, ProcessStatus processStatus) {
-
+        if(processStatus == ProcessStatus.TERMINATED) {
+            String uniqueName = mapProcessKeyToUniqueName.get(processKey);
+            CLIApp.send(CLIApp.KEY_TERM_EXEC, uniqueName, processKey);
+        }
     }
 }
